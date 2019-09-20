5d12eeb4dc0eb09fc2307504f644c45978dcea17
unknown <lucasmalucha@gmail.com>
2019-09-20 07:29:12 +0100

    data

--- questions/api/urls.py
+++ questions/api/urls.py
@@ -7,6 +7,8 @@ router.register(r"questions", view.QuestionViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("questions/<slug:slug>/answer/", view.AnswerListAPIView.as_view(), name="answer-list"),
-    path("questions/<slug:slug>/answer/", view.AnswerCreateAPIView.as_view(), name="create-answer")
+    path("questions/<slug:slug>/answers/", view.AnswerListAPIView.as_view(), name="answer-list"),
+    path("questions/<slug:slug>/answer/", view.AnswerCreateAPIView.as_view(), name="answer-create"),
+    path("answers/<int:pk>/", view.AnswerRUDAPIView.as_view(), name="answer-detail"),
+    path("answers/<int:pk>/like/", view.AnswerLikeAPIView.as_view(), name="answer-like")
 ]
