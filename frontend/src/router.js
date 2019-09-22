34d2bb9ed8246cc4e100867b156c280848e2d685
unknown <lucasmalucha@gmail.com>
2019-09-22 18:09:13 +0100

    vue router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Example from "./views/Example.vue";
+import About from "./views/About.vue";
 
 Vue.use(Router);
 
@@ -13,14 +15,16 @@ export default new Router({
       name: "home",
       component: Home
     },
+        {
+      path: "/example",
+      name: "example",
+      component: Example
+    },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
+
     }
   ]
 });
