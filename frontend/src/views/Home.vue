c1b884161c8447af9e448b150aeedcb3fff8cb99
unknown <lucasmalucha@gmail.com>
2019-09-24 20:24:24 +0100

    load more

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="container">
+    <div class="container mt-2">
       <div v-for="question in questions"
           :key="question.pk">
         <p class="mb-0">Posted by:
@@ -14,6 +14,12 @@
         <p>Answers: {{ question.answers_count }}</p>
         <hr>
       </div>
+      <div class="my-4">
+         <p v-show="loadingQuestions">...loading...</p>
+        <button v-show="next" @click="getQuestions" class="btn btn-sm btn-outline-success">
+          Load More
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -24,16 +30,29 @@ export default {
   name: "home",
   data() {
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions: false
     }
   },
   methods: {
     getQuestions() {
       let endpoint = "api/questions/";
+      if (this.next) {
+          endpoint = this.next;
+      }
+      this.loadingQuestions = true;
       // Use API Service on the endpoint
       apiService(endpoint)
         .then(data => {
-          this.questions.push(...data.results);
+          this.questions.push(...data.results)
+          this.loadingQuestions = false;
+          if (data.next) {
+            this.next = data.next;
+          }
+          else {
+            this.next = null;
+          }
         })
     }
   },
--- frontend/src/views/QuestionEditor.vue
+++ frontend/src/views/QuestionEditor.vue
@@ -17,7 +17,7 @@
 
 
 <script>
-import { apiService } from "@/common/api.service.js"
+import { apiService } from "@/common/api.service.js";
 export default {
     name: "QuestionEditor",
     data() {
