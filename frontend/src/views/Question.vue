b439d79ca09093a1b63b5b5e02b07a3303468225
unknown <lucasmalucha@gmail.com>
2019-09-25 07:10:35 +0100

    answer vue

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -7,11 +7,19 @@
             </p>
             <p> {{ question.created_at }}</p>
         </div>
+        <hr>
+        <div class="container">
+            <AnswerComponent v-for="(answer, index) in answers"
+             :answer="answer"
+             :key="index"
+            />
+        </div>
     </div>
 </template>
 
 <script>
-import { apiService } from "../common/api.service.js"
+import { apiService } from "@/common/api.service.js"
+import AnswerComponent from "@/components/Answer.vue";
 export default {
     name: "Question",
     props: {
@@ -20,9 +28,13 @@ export default {
             required: true
         }
     },
+    components: {
+        AnswerComponent
+    },
     data() {
         return {
-            question: {}
+            question: {},
+            answers: []
         }
     },
     methods: {
@@ -36,10 +48,18 @@ export default {
                 this.question = data;
                 this.setPageTitle(data.content)
             })
+        },
+        getQuestionAnswers() {
+            let endpoint = `/api/questions/${this.slug}/answers/`;
+            apiService(endpoint)
+            .then(data =>{
+                this.answers = data.results;
+            })
         }
     },
     created() {
         this.getQuestionData()
+        this.getQuestionAnswers()
     }
 }
 
